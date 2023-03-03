24b3969fa81bb6b78a67a2a25ca7a37d1d2365ae
Isabel Amaral <isabel.andre.amaral@gmail.com>
2023-03-03 16:24:57 +0000

    Added missing grammar rule initializations to Grammar Test

--- test/pt/up/fe/comp/GrammarTest.java
+++ test/pt/up/fe/comp/GrammarTest.java
@@ -17,10 +17,9 @@ import org.junit.Test;
 
 public class GrammarTest {
 
-
-    private static final String IMPORT = "";
-    private static final String MAIN_METHOD = "";
-    private static final String INSTANCE_METHOD = "";
+    private static final String IMPORT = "importDeclaration";
+    private static final String MAIN_METHOD = "methodDeclaration";
+    private static final String INSTANCE_METHOD = "methodDeclaration";
     private static final String STATEMENT = "statement";
     private static final String EXPRESSION = "expression";
 
@@ -44,10 +43,12 @@ public class GrammarTest {
         TestUtils.parseVerbose("class Foo {int a; int[] b; int c; boolean d; Bar e;}");
     }
 
-   /*@Test
+   /*
+   @Test
     public void testVarDeclString() {
         TestUtils.parseVerbose("String aString;", "VarDecl");
-    }*/
+    }
+    */
 
     @Test
     public void testMainMethodEmpty() {
@@ -56,8 +57,7 @@ public class GrammarTest {
 
     @Test
     public void testInstanceMethodEmpty() {
-        TestUtils.parseVerbose("int foo(int anInt, int[] anArray, boolean aBool, String aString) {return a;}",
-                INSTANCE_METHOD);
+        TestUtils.parseVerbose("int foo(int anInt, int[] anArray, boolean aBool, String aString) {return a;}", INSTANCE_METHOD);
     }
 
     @Test
@@ -239,5 +239,4 @@ public class GrammarTest {
     public void testExprChain() {
         TestUtils.parseVerbose("1 && 2 < 3 + 4 - 5 * 6 / 7", EXPRESSION);
     }
-
 }
