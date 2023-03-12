5a50c76e32227fb7aed01bac2a4864acd5c799a1
Milena Gouveia <up202008862@g.uporto.pt>
2023-03-12 12:09:07 +0000

    Implemented setters in MethodTable class

--- src/main/pt/up/fe/comp2023/MethodTable.java
+++ src/main/pt/up/fe/comp2023/MethodTable.java
@@ -7,23 +7,39 @@ import java.util.List;
 
 public class MethodTable {
     String name;
-    List<Symbol> parameters = new ArrayList<>();
-    List<Symbol> localVariables = new ArrayList<>();
+    List<Symbol> parameters;
+    List<Symbol> localVariables;
     Type returnType;
 
     public String getName() {
         return name;
     }
 
+    public void setName(String name) {
+        this.name = name;
+    }
+
     public List<Symbol> getParameters() {
         return parameters;
+    }
+
+    public void setParameters(List<Symbol> parameters) {
+        this.parameters = parameters;
     }
 
     public List<Symbol> getLocalVariables() {
         return localVariables;
     }
 
+    public void setLocalVariables(List<Symbol> localVariables) {
+        this.localVariables = localVariables;
+    }
+
     public Type getReturnType() {
         return returnType;
+    }
+
+    public void setReturnType(Type returnType) {
+        this.returnType = returnType;
     }
 }
