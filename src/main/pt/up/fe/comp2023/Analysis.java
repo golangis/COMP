19052eb85b9429a3dfd02c2ed93eb24cc3925579
Milena Gouveia <up202008862@g.uporto.pt>
2023-04-06 16:41:16 +0100

    Made symbolTable and reports list private fields in Analysis class

--- src/main/pt/up/fe/comp2023/Analysis.java
+++ src/main/pt/up/fe/comp2023/Analysis.java
@@ -8,9 +8,11 @@ import pt.up.fe.comp.jmm.report.Report;
 import java.util.ArrayList;
 
 public class Analysis implements JmmAnalysis {
+    private MySymbolTable symbolTable;
+    private final ArrayList<Report> reports = new ArrayList<>();
     @Override
     public JmmSemanticsResult semanticAnalysis(JmmParserResult parserResult) {
-        MySymbolTable symbolTable = new MySymbolTable(parserResult.getRootNode());
-        return new JmmSemanticsResult(parserResult, symbolTable, new ArrayList<Report>());
+        this.symbolTable = new MySymbolTable(parserResult.getRootNode());
+        return new JmmSemanticsResult(parserResult, this.symbolTable, this.reports);
     }
 }
