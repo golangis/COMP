a7539ebf966c27e6194e88d1d2ec80d89939eb72
Isabel Amaral <isabel.andre.amaral@gmail.com>
2023-04-06 15:59:17 +0000

    Implemented method to create super directive

--- src/main/pt/up/fe/comp2023/JasminUtils.java
+++ src/main/pt/up/fe/comp2023/JasminUtils.java
@@ -4,6 +4,7 @@ import org.specs.comp.ollir.AccessModifiers;
 import org.specs.comp.ollir.ClassUnit;
 
 public class JasminUtils {
+
     public static String createClassDirective(ClassUnit classUnit) {
         String classDirective = ".class ";
         if (classUnit.getClassAccessModifier() == AccessModifiers.PUBLIC)
@@ -13,6 +14,17 @@ public class JasminUtils {
         if (classUnit.getPackage() != null)
             classDirective += classUnit.getPackage() + '/';
         classDirective += classUnit.getClassName();
+
         return classDirective + '\n';
+    }
+
+    public static String createSuperDirective(ClassUnit classUnit) {
+        String superClassDirective = ".super ";
+        if (classUnit.getSuperClass() != null)
+            superClassDirective += classUnit.getSuperClass();
+        else
+            superClassDirective += "java/lang/Object";
+
+        return  superClassDirective + '\n';
     }
 }
