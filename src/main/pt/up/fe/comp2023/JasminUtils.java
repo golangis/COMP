c0cf682b434d8d16ff3019054a8819bf3dcb1097
Isabel Amaral <isabel.andre.amaral@gmail.com>
2023-04-07 11:50:07 +0000

    Implemented method to create field directive

--- src/main/pt/up/fe/comp2023/JasminUtils.java
+++ src/main/pt/up/fe/comp2023/JasminUtils.java
@@ -2,8 +2,14 @@ package pt.up.fe.comp2023;
 
 import org.specs.comp.ollir.AccessModifiers;
 import org.specs.comp.ollir.ClassUnit;
+import org.specs.comp.ollir.Field;
+import org.specs.comp.ollir.Type;
 
 public class JasminUtils {
+
+    public static String getTypeDescriptor(Type type) {
+        return "";
+    }
 
     public static String createClassDirective(ClassUnit classUnit) {
         String classDirective = ".class ";
@@ -26,5 +32,20 @@ public class JasminUtils {
             superClassDirective += "java/lang/Object";
 
         return  superClassDirective + '\n';
+    }
+
+    public static String createFieldDirective(Field field) {
+        String fieldDirective = ".field ";
+        fieldDirective += field.getFieldAccessModifier().name() + " ";
+        if (field.isStaticField())
+            fieldDirective += "static ";
+        if (field.isFinalField())
+            fieldDirective += "final ";
+        fieldDirective += field.getFieldName() + " ";
+        fieldDirective += getTypeDescriptor(field.getFieldType());
+        if (field.isInitialized())
+            fieldDirective += " = " + field.getInitialValue();
+
+        return fieldDirective + '\n';
     }
 }
