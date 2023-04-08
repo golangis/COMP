581f81946fe42e137f670c0288d764782cdc7d28
Milena Gouveia <up202008862@g.uporto.pt>
2023-04-08 03:38:10 +0100

    Implemented SemanticUtils getIdentifierType()

--- src/main/pt/up/fe/comp2023/SemanticUtils.java
+++ src/main/pt/up/fe/comp2023/SemanticUtils.java
@@ -1,5 +1,8 @@
 package pt.up.fe.comp2023;
 
+import pt.up.fe.comp.jmm.analysis.table.Symbol;
+import pt.up.fe.comp.jmm.analysis.table.Type;
+
 import java.util.List;
 import java.util.Objects;
 
@@ -11,5 +14,30 @@ public class SemanticUtils {
                 return true;
         }
         return false;
+    }
+
+    public static Type getIdentifierType(String methodName, String identifier, MySymbolTable symbolTable){
+        List<Symbol> localVariables = symbolTable.getLocalVariables(methodName);
+        for (Symbol localVariable : localVariables) {
+            if(Objects.equals(localVariable.getName(), identifier))
+                return localVariable.getType();
+        }
+
+        List<Symbol> parameters = symbolTable.getParameters(methodName);
+        for (Symbol parameter : parameters) {
+            if(Objects.equals(parameter.getName(), identifier))
+                return parameter.getType();
+        }
+
+        List<Symbol> fields = symbolTable.getFields();
+        for (Symbol field : fields) {
+            if(Objects.equals(field.getName(), identifier))
+                return field.getType();
+        }
+
+        if(findImport(symbolTable.getImports(), identifier))
+            return new Type(identifier, false);
+
+        return new Type("unknown", false);
     }
 }
