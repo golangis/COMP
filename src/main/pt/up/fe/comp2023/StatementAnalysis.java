c09c9cbfbf6ede08e40b161855e3c8337c281ff6
Milena Gouveia <up202008862@g.uporto.pt>
2023-04-10 00:31:19 +0100

    Implemented checkBooleanCondition in StatementAnalysis

--- src/main/pt/up/fe/comp2023/StatementAnalysis.java
+++ src/main/pt/up/fe/comp2023/StatementAnalysis.java
@@ -1,15 +1,20 @@
 package pt.up.fe.comp2023;
 
+import pt.up.fe.comp.jmm.analysis.table.Type;
 import pt.up.fe.comp.jmm.ast.AJmmVisitor;
 import pt.up.fe.comp.jmm.ast.JmmNode;
 import pt.up.fe.comp.jmm.report.Report;
+import pt.up.fe.comp.jmm.report.ReportType;
+import pt.up.fe.comp.jmm.report.Stage;
 
 import java.util.List;
+
+import static pt.up.fe.comp2023.SemanticUtils.*;
 
 public class StatementAnalysis extends AJmmVisitor<Void, Void> {
     private final MySymbolTable symbolTable;
     private final List<Report> reports;
-    private ExpressionAnalysis expressionAnalysis;
+    private final ExpressionAnalysis expressionAnalysis;
 
     public StatementAnalysis(JmmNode statementNode,  MySymbolTable symbolTable, List<Report> reports){
         this.symbolTable = symbolTable;
@@ -37,7 +42,17 @@ public class StatementAnalysis extends AJmmVisitor<Void, Void> {
     }
 
     private Void checkBooleanCondition(JmmNode jmmNode, Void unused) {
-        //TODO
+        JmmNode expressionNode = jmmNode.getJmmChild(0);
+
+        Type conditionType = expressionAnalysis.visit(expressionNode);
+        if(conditionType.equals(BOOLEAN_TYPE)) {
+            String message = "Expected condition of type '" + BOOLEAN + "' but found '" + conditionType.print() + "'.";
+            this.reports.add(new Report(ReportType.ERROR, Stage.SEMANTIC, 1, 1, message)); //TODO: change line and column values
+        }
+
+        for(int i= 1; i < jmmNode.getNumChildren(); i++){
+            visit(jmmNode.getJmmChild(i));
+        }
         return null;
     }
 
