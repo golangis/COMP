49a1796be5172a0404d28c0ca88232dc19bec832
Isabel Amaral <isabel.andre.amaral@gmail.com>
2023-04-11 20:38:02 +0100

    Added verification if an element is literal in load implementation

--- src/main/pt/up/fe/comp2023/JVMInstructionUtils.java
+++ src/main/pt/up/fe/comp2023/JVMInstructionUtils.java
@@ -10,25 +10,28 @@ import static java.lang.Math.pow;
 
 public class JVMInstructionUtils {
 
-    public static String getLoadInstruction(ElementType elementType, Element element, HashMap<String, Descriptor> varTable) {
+    public static String getLoadInstruction(Element element, HashMap<String, Descriptor> varTable) {
+        if (element.isLiteral()) {
+            int literal = parseInt(((LiteralElement)element).getLiteral());
+            if (literal >= 0 && literal <= 5)
+                return "iconst_" + literal + '\n';
+            if (literal == -1)
+                return "iconst_m" + abs(literal) + '\n';
+            if (abs(literal) < pow(2, 7))
+                return "bipush " + literal + '\n';
+            if (abs(literal) < pow(2, 15))
+                return "sipush " + literal + '\n';
+            return "ldc " + literal + '\n';
+        }
+
+        ElementType elementType = element.getType().getTypeOfElement();
         switch (elementType) {
             case THIS:
                 return "aload_0\n";
             case STRING: case OBJECTREF: case ARRAYREF:
                 return "aload_" + varTable.get(((Operand)element).getName()).getVirtualReg()+ '\n';
-            case BOOLEAN:
+            case INT32: case BOOLEAN:
                 return "iload_" + varTable.get(((Operand)element).getName()).getVirtualReg() + '\n';
-            case INT32:
-                int literal = parseInt(((LiteralElement)element).getLiteral());
-                if (literal >= 0 && literal <= 5)
-                    return "iconst_" + literal + '\n';
-                if (literal == -1)
-                    return "iconst_m" + abs(literal) + '\n';
-                if (abs(literal) < pow(2, 7))
-                    return "bipush " + literal + '\n';
-                if (abs(literal) < pow(2, 15))
-                    return "sipush " + literal + '\n';
-                return "ldc " + literal + '\n';
         }
         return "";
     }
@@ -42,11 +45,11 @@ public class JVMInstructionUtils {
             case VOID:
                 return "return\n";
             case INT32: case BOOLEAN:
-                statement += getLoadInstruction(returnType, returnElement, varTable);
+                statement += getLoadInstruction(returnElement, varTable);
                 statement += "ireturn\n";
                 break;
             case STRING: case OBJECTREF: case ARRAYREF: case THIS:
-                statement += getLoadInstruction(returnType, returnElement, varTable);
+                statement += getLoadInstruction(returnElement, varTable);
                 statement += "areturn\n";
         }
         return statement;
