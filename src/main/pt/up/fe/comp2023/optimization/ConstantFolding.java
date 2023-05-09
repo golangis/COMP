0799f2de37d29e401b177f38aa90b28e539c67cd
Milena Gouveia <up202008862@g.uporto.pt>
2023-05-09 23:34:20 +0100

    Add default visitor of ConstantFolding class

--- src/main/pt/up/fe/comp2023/optimization/ConstantFolding.java
+++ src/main/pt/up/fe/comp2023/optimization/ConstantFolding.java
@@ -3,17 +3,62 @@ package pt.up.fe.comp2023.optimization;
 import pt.up.fe.comp.jmm.analysis.JmmSemanticsResult;
 import pt.up.fe.comp.jmm.analysis.table.SymbolTable;
 import pt.up.fe.comp.jmm.ast.AJmmVisitor;
-import pt.up.fe.comp2023.semantic.MySymbolTable;
+import pt.up.fe.comp.jmm.ast.JmmNode;
 
 public class ConstantFolding extends AJmmVisitor<Void, Void> {
+    private JmmSemanticsResult semanticsResult;
     private SymbolTable symbolTable;
     private String currentMethodName;
+    private boolean codeModified;
     public ConstantFolding (JmmSemanticsResult semanticsResult){
+        this.semanticsResult = semanticsResult;
         this.symbolTable = semanticsResult.getSymbolTable();
+    }
+
+    public boolean apply(){
+        this.codeModified = false;
+        visit(semanticsResult.getRootNode());
+        return this.codeModified;
     }
 
     @Override
     protected void buildVisitor() {
+        setDefaultVisit(this::setDefaultVisit);
+        addVisit("ParenthesesExpr", this::computeParenthesesExprResult);
+        addVisit("NegationExpr", this::negateBooleanExpr);
+        addVisit("ArithmeticExpr", this::computeArithmeticExprResult);
+        addVisit("ComparisonExpr", this::computeComparisonResult);
+        addVisit("LogicalExpr", this::computeLogicalExprResult);
+    }
 
+    private Void setDefaultVisit(JmmNode jmmNode, Void unused) {
+        for (JmmNode child: jmmNode.getChildren())
+            visit(child);
+        return null;
+    }
+
+    private Void computeParenthesesExprResult(JmmNode jmmNode, Void unused) {
+        //TODO
+        return null;
+    }
+
+    private Void negateBooleanExpr(JmmNode jmmNode, Void unused) {
+        //TODO
+        return null;
+    }
+
+    private Void computeArithmeticExprResult(JmmNode jmmNode, Void unused) {
+        //TODO
+        return null;
+    }
+
+    private Void computeComparisonResult(JmmNode jmmNode, Void unused) {
+        //TODO
+        return null;
+    }
+
+    private Void computeLogicalExprResult(JmmNode jmmNode, Void unused) {
+        //TODO
+        return null;
     }
 }
