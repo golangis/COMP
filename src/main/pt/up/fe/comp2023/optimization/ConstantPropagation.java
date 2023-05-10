4a1fba6e8c6cedbaed794ae2d0fc12419567cdc1
Milena Gouveia <up202008862@g.uporto.pt>
2023-05-10 01:49:13 +0100

    Add default visitor of ConstantPropagation

--- src/main/pt/up/fe/comp2023/optimization/ConstantPropagation.java
+++ src/main/pt/up/fe/comp2023/optimization/ConstantPropagation.java
@@ -3,17 +3,32 @@ package pt.up.fe.comp2023.optimization;
 import pt.up.fe.comp.jmm.analysis.JmmSemanticsResult;
 import pt.up.fe.comp.jmm.analysis.table.SymbolTable;
 import pt.up.fe.comp.jmm.ast.AJmmVisitor;
+import pt.up.fe.comp.jmm.ast.JmmNode;
 
 public class ConstantPropagation extends AJmmVisitor<Void, Void> {
     private SymbolTable symbolTable;
-    private String currentMethodName;
+    private final JmmSemanticsResult semanticsResult;
+    private boolean codeModified;
 
     public ConstantPropagation (JmmSemanticsResult semanticsResult){
         this.symbolTable = semanticsResult.getSymbolTable();
+        this.semanticsResult = semanticsResult;
+    }
+
+    public boolean apply(){
+        this.codeModified = false;
+        visit(semanticsResult.getRootNode());
+        return this.codeModified;
     }
 
     @Override
     protected void buildVisitor() {
+        setDefaultVisit(this::setDefaultVisit);
+    }
 
+    private Void setDefaultVisit(JmmNode jmmNode, Void unused) {
+        for (JmmNode child: jmmNode.getChildren())
+            visit(child);
+        return null;
     }
 }
