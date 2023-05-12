aeb9638ae894299386b0d427a482f10afa480093
Milena Gouveia <up202008862@g.uporto.pt>
2023-05-12 03:15:53 +0100

    Add visitor for a method declaration in ConstantPropagation

--- src/main/pt/up/fe/comp2023/optimization/ConstantPropagation.java
+++ src/main/pt/up/fe/comp2023/optimization/ConstantPropagation.java
@@ -1,6 +1,7 @@
 package pt.up.fe.comp2023.optimization;
 
 import pt.up.fe.comp.jmm.analysis.JmmSemanticsResult;
+import pt.up.fe.comp.jmm.analysis.table.SymbolTable;
 import pt.up.fe.comp.jmm.ast.AJmmVisitor;
 import pt.up.fe.comp.jmm.ast.JmmNode;
 
@@ -9,10 +10,13 @@ import java.util.Map;
 
 public class ConstantPropagation extends AJmmVisitor<Map<String, String>, Void> {
     private final JmmSemanticsResult semanticsResult;
+    private final SymbolTable symbolTable;
+    private String currentMethodName;
     private boolean codeModified;
 
     public ConstantPropagation (JmmSemanticsResult semanticsResult){
         this.semanticsResult = semanticsResult;
+        this.symbolTable = semanticsResult.getSymbolTable();
     }
 
     public boolean apply(){
@@ -26,6 +30,9 @@ public class ConstantPropagation extends AJmmVisitor<Map<String, String>, Void> 
     @Override
     protected void buildVisitor() {
         setDefaultVisit(this::setDefaultVisit);
+        addVisit("MethodDecl", this::changeCurrentMethodName);
+        addVisit("VoidMethodDecl", this::changeCurrentMethodName);
+        addVisit("MainMethodDecl", this::changeCurrentMethodName);
         addVisit("Assignment", this::dealWithAssignment);
         addVisit("Identifier", this::dealWithIdentifier);
     }
@@ -36,10 +43,19 @@ public class ConstantPropagation extends AJmmVisitor<Map<String, String>, Void> 
         return null;
     }
 
+    private Void changeCurrentMethodName(JmmNode jmmNode, Map<String, String> constants) {
+        this.currentMethodName = jmmNode.get("methodname");
+        constants.clear();
+
+        for (JmmNode child: jmmNode.getChildren())
+            visit(child, constants);
+        return null;
+    }
+
     private Void dealWithAssignment(JmmNode jmmNode, Map<String, String> constants) {
         String varName = jmmNode.get("varname");
         JmmNode exprNode = jmmNode.getJmmChild(0);
-        visit(exprNode);
+        visit(exprNode, constants);
 
         if (exprNode.getKind().equals("Integer") || exprNode.getKind().equals("Boolean"))
             constants.put(varName, exprNode.get("value"));
@@ -49,7 +65,7 @@ public class ConstantPropagation extends AJmmVisitor<Map<String, String>, Void> 
     }
 
     private Void dealWithIdentifier(JmmNode jmmNode, Map<String, String> constants) {
-        //TODO
+        String identifierName = jmmNode.get("value");
 
         return null;
     }
