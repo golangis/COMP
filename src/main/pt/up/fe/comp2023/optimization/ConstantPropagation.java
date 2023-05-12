da25fc5bb1ce77b9dc98758731ba42b98ed2cfdc
Milena Gouveia <up202008862@g.uporto.pt>
2023-05-12 02:50:38 +0100

    Improved ConstantPropagation in order to store constants

--- src/main/pt/up/fe/comp2023/optimization/ConstantPropagation.java
+++ src/main/pt/up/fe/comp2023/optimization/ConstantPropagation.java
@@ -1,34 +1,52 @@
 package pt.up.fe.comp2023.optimization;
 
 import pt.up.fe.comp.jmm.analysis.JmmSemanticsResult;
-import pt.up.fe.comp.jmm.analysis.table.SymbolTable;
 import pt.up.fe.comp.jmm.ast.AJmmVisitor;
 import pt.up.fe.comp.jmm.ast.JmmNode;
 
-public class ConstantPropagation extends AJmmVisitor<Void, Void> {
-    private SymbolTable symbolTable;
+import java.util.HashMap;
+import java.util.Map;
+
+public class ConstantPropagation extends AJmmVisitor<Map<String, String>, Void> {
     private final JmmSemanticsResult semanticsResult;
     private boolean codeModified;
 
     public ConstantPropagation (JmmSemanticsResult semanticsResult){
-        this.symbolTable = semanticsResult.getSymbolTable();
         this.semanticsResult = semanticsResult;
     }
 
     public boolean apply(){
         this.codeModified = false;
-        visit(semanticsResult.getRootNode());
+        Map<String, String> constants = new HashMap<>();
+
+        visit(semanticsResult.getRootNode(), constants);
         return this.codeModified;
     }
 
     @Override
     protected void buildVisitor() {
         setDefaultVisit(this::setDefaultVisit);
+        addVisit("Assignment", this::dealWithAssignment);
+        addVisit("Identifier", this::dealWithIdentifier);
     }
 
-    private Void setDefaultVisit(JmmNode jmmNode, Void unused) {
+    private Void setDefaultVisit(JmmNode jmmNode, Map<String, String> constants) {
         for (JmmNode child: jmmNode.getChildren())
-            visit(child);
+            visit(child, constants);
         return null;
     }
+
+    private Void dealWithAssignment(JmmNode jmmNode, Map<String, String> stringStringMap) {
+        String varName = jmmNode.get("varname");
+        //TODO
+        return null;
+    }
+
+    private Void dealWithIdentifier(JmmNode jmmNode, Map<String, String> stringStringMap) {
+        //TODO
+
+        return null;
+    }
+
+
 }
