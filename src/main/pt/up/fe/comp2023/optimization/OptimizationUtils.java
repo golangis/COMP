5742fac288b0460d2e4adb114eb8ee69400095a7
Milena Gouveia <up202008862@g.uporto.pt>
2023-05-22 22:50:40 +0100

    Add utils methods compute union and difference of sets

--- src/main/pt/up/fe/comp2023/optimization/OptimizationUtils.java
+++ src/main/pt/up/fe/comp2023/optimization/OptimizationUtils.java
@@ -1,6 +1,10 @@
 package pt.up.fe.comp2023.optimization;
 
+import org.specs.comp.ollir.Element;
+
+import java.util.HashSet;
 import java.util.Map;
+import java.util.Set;
 
 public class OptimizationUtils {
     public static void intersectMaps (Map<String, String> map1, Map<String, String> map2, Map<String, String> result){
@@ -13,5 +17,19 @@ public class OptimizationUtils {
             if (map2.containsKey(key) && map2.get(key).equals(value))
                 result.put(key, value);
         }
+    }
+
+    public static Set<Element> differenceSets (Set<Element> set1, Set<Element> set2){
+        Set<Element> result = new HashSet<>(set1);
+        result.removeAll(set2);
+
+        return result;
+    }
+
+    public static Set<Element> unionSets (Set<Element> set1, Set<Element> set2){
+        Set<Element> result = new HashSet<>(set1);
+        result.addAll(set2);
+
+        return result;
     }
 }
